e7f6a584c13a75ae18354aa8e0569d417f5a3f08
gergely <kiss-gergely@outlook.com>
2018-11-18 10:20:51 +0000

    .gitignore is now working

--- src/main/java/uk/gergely/kiss/springbootquickstart/topic/TopicRepositoryService.java
+++
@@ -1,7 +0,0 @@
-package uk.gergely.kiss.springbootquickstart.topic;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface TopicRepositoryService extends CrudRepository<Topic, String>{ 
-
-}
